dc08c571ec6eb060f720ce9f3dd963f9cc32252e
kkellybonilla <kkelly.bonilla@stanford.edu>
2023-03-28 03:22:57 -0400

    Initial Commit

---
+++ BeReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeReal
+//
+//  Created by Kelly Bonilla Guzmán on 3/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
